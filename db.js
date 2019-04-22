4b6442b2d86703544778469e8bb8e01408254ffb
Becky <you@example.com>
2019-04-22 10:13:48 -0400

    update

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 let Sequelize = require('sequelize')
 
-const sequelize = new Sequelize(process.env.NAME, 'postgres', process.env.PASS, {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres'
 })
 
